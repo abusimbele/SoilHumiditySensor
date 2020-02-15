bc7561ca4e43d4c4fcafab66a5f0ff53fc4735eb
Sascha Friedrich <sascha.friedrich.email@gmail.com>
2020-02-15 11:48:47 +0000

    added calibration modes because there are problems when you use a powerbank instead of the PC ...

--- humiditySensor.ino
+++ humiditySensor.ino
@@ -3,8 +3,7 @@ not for layers,
 not for partially put in ...
 
 
-Look what you use for your calibration...
-
+I use 2 buttons at 6,7 for triggering the calibration modes
 
 */
 
@@ -27,8 +26,8 @@ int sensorPin = A0;  // select the input pin for the potentiometer  control the 
 Your Calibration measurements put in here
 */
 
-const float MAX_ANALOG_SENSOR_VALUE = 574; // This is the MIN humidity
-const float MIN_ANALOG_SENSOR_VALUE = 230;//This is the MAX humidity  ... yes it's reciprocal ... 
+float MAX_ANALOG_SENSOR_VALUE = 574; // This is the MIN humidity
+float MIN_ANALOG_SENSOR_VALUE = 230;//This is the MAX humidity  ... yes it's reciprocal ... 
 
 
 
@@ -36,7 +35,8 @@ const float MIN_ANALOG_SENSOR_VALUE = 230;//This is the MAX humidity  ... yes it
 Global variables
 */
 float sensorValue = 0;  // variable to store the value coming from the sensor
-
+int pinCalibrateHigh = 6; // button to start the calibration for low humidity
+int pinCalibrateLow = 7;  // button to start the calibration for lhigh humidity
 
 
 
@@ -45,9 +45,11 @@ void setup() {
   
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
+  pinMode(pinCalibrateHigh, INPUT);
+  pinMode(pinCalibrateLow, INPUT);
   
   //pinMode(ledPin, OUTPUT);  
-  Serial.begin(9600);
+  //Serial.begin(9600);
   
 }
 
@@ -59,24 +61,58 @@ void setup() {
 
 
 void loop() {
+  
+  
+  sensorValue = analogRead(sensorPin);   // read the value from the sensor:
+  
+  
+  
+  if(digitalRead(pinCalibrateHigh)){
+    lcd.clear(); 
+    MIN_ANALOG_SENSOR_VALUE = sensorValue;
+    lcd.print("AnalogIn: "); 
+    lcd.print(MIN_ANALOG_SENSOR_VALUE); 
+      delay(2000);
+    
+    
+    
+  
+  }
+  
+  
+    if(digitalRead(pinCalibrateLow)){
+      MAX_ANALOG_SENSOR_VALUE = sensorValue;
+      lcd.clear(); 
+      lcd.print("AnalogIn: "); 
+      lcd.print(MAX_ANALOG_SENSOR_VALUE); 
+      delay(5000);
+  }
  
  
-  sensorValue = analogRead(sensorPin);   // read the value from the sensor:   
+ 
+    
+  lcd.print("AnalogIn: "); 
+  lcd.print(sensorValue,0);
+  lcd.setCursor(0, 1); 
   sensorValue = 100-(((sensorValue - MIN_ANALOG_SENSOR_VALUE)/(MAX_ANALOG_SENSOR_VALUE - MIN_ANALOG_SENSOR_VALUE))*100);  // convert into %
+  
+  
   
   
   
   /*
   Serial output
   */
-  Serial.print("Feuchte in %: ");
-  Serial.println(sensorValue);
+  //Serial.print("Feuchte in %: ");
+  //Serial.println(sensorValue);
   
   
     /*
   LCD output
   */
+  lcd.print("Humidity: ");
   lcd.print(sensorValue,2);  
+  lcd.print("%"); 
 
   
   
